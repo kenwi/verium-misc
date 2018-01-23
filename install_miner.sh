7905bf4929608fd4551ba4fad0b52c33801dcfe4
kenwi <kenneth@wilhelmsen.nu>
2018-01-23 14:19:05 +0100

    Create install_miner.sh

---
+++ install_miner.sh
@@ -0,0 +1,13 @@
+sudo apt update -y
+sudo apt install git -y
+
+git clone https://github.com/fireworm71/veriumMiner
+
+sudo apt install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev -y
+
+cd veriumMiner
+./build.sh
+echo "./cpuminer -n 1048576 -o stratum+tcp://eu.vrm.mining-pool.ovh:3032 -u WfqGj6Cp2lW8tabDCT_2.$(hostname) -p p2lW8tabDCTXf $@" > run.sh
+chmod +x run.sh
+
+echo "Execute run.sh to start the miner. Additional parameters will be passed through to the cpuminer executable."
