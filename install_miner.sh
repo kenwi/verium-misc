a49dc6905499c204855271f811bcaa3d44d31693
kenwi <kenneth@wilhelmsen.nu>
2018-01-25 18:46:06 +0100

    Update install_miner.sh

--- install_miner.sh
+++ install_miner.sh
@@ -11,13 +11,12 @@ git clone https://github.com/fireworm71/veriumMiner
 sudo apt install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev -y
 
 # Build and output some guidance
-cd veriumMiner 
 ./build.sh
 echo "./cpuminer -n 1048576 -o stratum+tcp://ssh.wilhelmsen.nu:3332 -u $USER.$(hostname) -p $PASSWORD $@" > run.sh
 chmod +x run.sh
 
-cd veriumMiner
 echo ""
+echo "cd veriumMiner/ to enter miner directory"
 echo "Start the miner with ./run.sh, additional parameters will be passed through to the cpuminer executable."
 echo "Example"
 echo "* To limit to the number of N threads: ./run.sh -t 1"
