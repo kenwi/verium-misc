03c46eaff826c74ec6eb77bddece51b10e318608
kenwi <kenneth@wilhelmsen.nu>
2018-01-25 18:48:10 +0100

    Update install_miner.sh

--- install_miner.sh
+++ install_miner.sh
@@ -12,11 +12,11 @@ sudo apt install automake autoconf pkg-config libcurl4-openssl-dev libjansson-de
 
 # Build and output some guidance
 ./build.sh
-echo "./cpuminer -n 1048576 -o stratum+tcp://ssh.wilhelmsen.nu:3332 -u $USER.$(hostname) -p $PASSWORD $@" > run.sh
-chmod +x run.sh
+echo "cd veriumMiner" >> ../run.sh
+echo "./cpuminer -n 1048576 -o stratum+tcp://ssh.wilhelmsen.nu:3332 -u $USER.$(hostname) -p $PASSWORD $@" >> ../ run.sh
+chmod +x ../run.sh
 
 echo ""
-echo "cd veriumMiner/ to enter miner directory"
 echo "Start the miner with ./run.sh, additional parameters will be passed through to the cpuminer executable."
 echo "Example"
 echo "* To limit to the number of N threads: ./run.sh -t 1"
