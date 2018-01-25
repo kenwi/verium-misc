691099c6f955357617639281837c12993e53306f
kenwi <kenneth@wilhelmsen.nu>
2018-01-25 18:08:54 +0100

    Update install_miner.sh

--- install_miner.sh
+++ install_miner.sh
@@ -1,10 +1,12 @@
+# Update apt and install git
 sudo apt update -y
 sudo apt install git -y
 
+# Clone miner and install deps
 git clone https://github.com/fireworm71/veriumMiner
-
 sudo apt install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev -y
 
+# Build and output some guidance
 cd veriumMiner
 ./build.sh
 echo "./cpuminer -n 1048576 -o stratum+tcp://ssh.wilhelmsen.nu:3332 -u myusername.$(hostname) -p pasword $@" > run.sh
