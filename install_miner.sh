3e2c6d5c8cf2bad1077aed3d78272d825a08835d
kenwi <kenneth@wilhelmsen.nu>
2018-01-25 18:28:49 +0100

    Update install_miner.sh

--- install_miner.sh
+++ install_miner.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+read -p "Enter pool username: " USER
+read -p "Worker name is $(hostname). Enter worker password: " PASSWORD
+
 # Update apt and install git
 sudo apt update -y
 sudo apt install git -y
@@ -7,12 +11,12 @@ git clone https://github.com/fireworm71/veriumMiner
 sudo apt install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev -y
 
 # Build and output some guidance
-cd veriumMiner
-./build.sh
-echo "./cpuminer -n 1048576 -o stratum+tcp://ssh.wilhelmsen.nu:3332 -u myusername.$(hostname) -p pasword $@" > run.sh
+cd veriumMiner ./build.sh
+echo "./cpuminer -n 1048576 -o stratum+tcp://ssh.wilhelmsen.nu:3332 -u $USER.$(hostname) -p $PASSWORD $@" > run.sh
 chmod +x run.sh
 
 echo ""
-echo "Edit and execute run.sh to start the miner. Additional parameters will be passed through to the cpuminer executable. Default worker username is system hostname."
-echo "Example: To limit to the number of N threads: ./run.sh -t 1"
+echo "Additional parameters will be passed through to the cpuminer executable."
+echo "Example"
+echo "* To limit to the number of N threads: ./run.sh -t 1"
 echo ""
